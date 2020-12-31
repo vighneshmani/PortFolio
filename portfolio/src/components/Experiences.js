f4a391ec64574ec2723434214757a506bf7c3db2
Vighnesh <vighneshmani00@gmail.com>
2020-12-31 20:57:15 +0530

    Experience section added

---
+++ portfolio/src/components/Experiences.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './Carditem';
+
+function Experiences() {
+  return (
+    <div className='cards'>
+      <h1>My Experiences</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/MMB.jpg'
+              text='Digital Marketing Intern'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='images/VMGuitar.jpg'
+              text='Guitar Instructor'
+              label='Adventure'
+              path='/products'
+            />
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Experiences;
--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -3,13 +3,14 @@ import '../../App.css';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
 import ProjectCards from '../Cards';
-
+import Experiences from '../Experiences';
 
 function Home() {
     return (
         <>
             <HeroSection />
             <ProjectCards/>
+            <Experiences/>
             <Footer />
 
         </>
